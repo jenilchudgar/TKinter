01b758d09740172e87dd7565989e752db747b4d7
Jenil Chudgar <jenilchudgarfamily@gmail.com>
2021-12-21 20:05:33 +0530

    File

--- #41 Drag and Drop Images With The Mouse.py
+++ #41 Drag and Drop Images With The Mouse.py
@@ -13,11 +13,18 @@ y = h//2
 
 canvas = Canvas(root,width=w,height=h,bg="white")
 canvas.pack(pady=10)
-
+global my_img,img
 img = PhotoImage(file="melon.png")
 
 my_img = canvas.create_image(0,0,anchor=NW,image=img)
 
-root.bind('<>')
+def move(e:Event):
+    global my_img,img
+    x = e.x 
+    y = e.y
+    img = PhotoImage(file="melon.png")
+    my_img = canvas.create_image(x,y,image=img)
+
+root.bind('<B1-Motion>',move)
 
 root.mainloop()
