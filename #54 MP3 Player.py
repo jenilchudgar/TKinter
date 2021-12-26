79a47fe2a05182f05c4b22d6c95eec38436b0794
Jenil Chudgar <jenilchudgarfamily@gmail.com>
2021-12-26 15:27:35 +0530

    Changes

--- #54 MP3 Player.py
+++ #54 MP3 Player.py
@@ -4,7 +4,7 @@ import pygame
 root = Tk()
 root.title("MP3 Player")
 root.iconbitmap("computer.ico")
-root.geometry("600x400")
+root.geometry("500x400")
 
 # Initialize Pygame Mixer
 pygame.mixer.init()
@@ -12,5 +12,30 @@ pygame.mixer.init()
 # Create Playlist Box
 song_box = Listbox(root,bg="black",fg="white",width=50)
 song_box.pack(pady=20)
+
+
+# Define Player Control Images
+back_img = PhotoImage(file="img/player/previous.png")
+forward_img = PhotoImage(file="img/player/forward.png")
+play_img = PhotoImage(file="img/player/play.png")
+pause_img = PhotoImage(file="img/player/pause.png")
+stop_img = PhotoImage(file="img/player/stop.png")
+
+# Create Player Control Frame
+control_frame = Frame(root)
+control_frame.pack()
+
+# Create Player Control Buttons
+back_btn = Button(control_frame,image=back_img,borderwidth=0)
+play_btn = Button(control_frame,image=play_img,borderwidth=0)
+pause_btn = Button(control_frame,image=pause_img,borderwidth=0)
+stop_btn = Button(control_frame,image=stop_img,borderwidth=0)
+forward_btn = Button(control_frame,image=forward_img,borderwidth=0)
+
+forward_btn.grid(row=0,column=0,padx=10)
+play_btn.grid(row=0,column=1,padx=10)
+pause_btn.grid(row=0,column=2,padx=10)
+stop_btn.grid(row=0,column=3,padx=10)
+back_btn.grid(row=0,column=4,padx=10)
 
 root.mainloop()
